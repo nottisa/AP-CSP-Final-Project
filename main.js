32ac3fbaad97a3fc5edccb58136b3f3565731295
nottisa <theonewhohasthisemail@gmail.com>
2024-02-28 13:01:04 +0000

    Remove exit

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const { exit } = require('node:process');
-
 //sort the input array alphabetically
 const sortJson = {'alphabetical': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], 'reverse alphabetical': ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']};
 const arrayToSort = ['zebra', 'cube', 'watermelon', 'apple', 'computer'];
@@ -40,7 +38,6 @@ function main() {
     userInput.question('How would you like to sort the above list? ' + generateTypeOptions().join(', ') + " >>> ", input => { //Node.js docs https://nodejs.org/en/learn/command-line/accept-input-from-the-command-line-in-nodejs
         let output = sortArray(arrayToSort, input);
         console.log('Here is the sorted array:\n' + output.join(', '));
-        exit();
     })
 }
 
