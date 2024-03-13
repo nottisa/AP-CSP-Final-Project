ad6a5770cacd55f486be8e0dc31a338e53d02639
nottisa <theonewhohasthisemail@gmail.com>
2024-03-13 12:22:41 +0000

    Fix problems with formatting and removed the problem child

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const sortJson = {'alphabetical': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], 'reverse alphabetical': ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']};
-const arrayToSort = ['zebra', 'cube', 'watermelon', 'apple', 'computer'];
+const arrayToSort = ['zebra', 'cube', 'watermelon', 'apple', 'bear'];
 const userInput = require('node:readline').createInterface({input: process.stdin, output: process.stdout}); 
 //This code was derived from  ^^ Node.js docs https://nodejs.org/en/learn/command-line/accept-input-from-the-command-line-in-nodejs
 
@@ -8,7 +8,6 @@ function sortArray(arrayToSort, type) {
     let arrayToSort2 = Object.assign([], arrayToSort);
     while (arrayToSort2.length > 0) {
         var lowestValue = '';
-        console.log(arrayToSort2);
         for (let i2 = 0; i2 < arrayToSort2.length; i2++) {
             if (lowestValue == '' || sortJson[type].indexOf(arrayToSort2[i2][0]) < sortJson[type].indexOf(lowestValue[0])) { 
                 lowestValue = arrayToSort2[i2];
@@ -38,7 +37,7 @@ function generateTypeOptions() {
 
 console.log('Here is the array sorted in reverse alphabetical order:\n' + (sortArray(arrayToSort, "reverse alphabetical")).join(', '));
 function main() {
-    console.log(arrayToSort);
+    console.log("\n\n" + arrayToSort.join(', '));
     userInput.question('How would you like to sort the above list? ' + generateTypeOptions().join(', ') + " >>> ", input => { 
         //This code was derived from  ^^ Node.js docs https://nodejs.org/en/learn/command-line/accept-input-from-the-command-line-in-nodejs
         let output = sortArray(arrayToSort, input);
